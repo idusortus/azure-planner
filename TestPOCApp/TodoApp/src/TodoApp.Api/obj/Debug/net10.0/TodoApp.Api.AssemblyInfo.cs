063810f2dcedf2188df9adf93fefa79a836e5730
Sam <122037802+sam-johnson-wi@users.noreply.github.com>
2026-01-17 21:39:19 -0600

    updated

--- TestPOCApp/TodoApp/src/TodoApp.Api/obj/Debug/net10.0/TodoApp.Api.AssemblyInfo.cs
+++ TestPOCApp/TodoApp/src/TodoApp.Api/obj/Debug/net10.0/TodoApp.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApp.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9aa9411b7586215a122983832fe02cfc02ee100d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dcd5f8a2b2abb3c3cdb3d624a26ee99e329c75e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApp.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApp.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
